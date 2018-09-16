6581fd392d6ea58a3c598f49ffd2b6f232f4ccdb
Danpatpang <puri8467@gmail.com>
2018-09-16 15:57:34 +0900

    write part1 source code

---
+++ part1/src/part1-1-3-2.js
@@ -0,0 +1,6 @@
+function run() {
+	console.log("3 second");
+}
+console.log("start");
+setTimeout(run,3000);
+console.log("finish");
---
+++ part1/src/part1-1-4-1.js
@@ -0,0 +1,6 @@
+function longRunningTask() {
+console.log('finish');
+}
+console.log('start');
+longRunningTask();
+console.log('next task');
