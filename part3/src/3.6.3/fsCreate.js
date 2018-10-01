cb1cafbf738305ede9dcb3b0e6cf8a3f2376eb2c
puri8467 <puri8467@gmail.com>
2018-10-01 23:57:49 +0900

    update content

--- part3/src/3.6.3/fsCreate.js
+++ part3/src/3.6.3/fsCreate.js
@@ -1,5 +1,6 @@
 const fs = require(`fs`);
 
+// 3 중에 하나는 실행되냐
 fs.access(`./folder`, fs.constants.F_OK | fs.constants.R_OK | fs.constants.W_OK, (err) => {
     if (err) {
         // 폴더 / 파일 없을 때 발생하는 에러
@@ -30,3 +31,16 @@ fs.access(`./folder`, fs.constants.F_OK | fs.constants.R_OK | fs.constants.W_OK,
         console.log(`이미 폴더 존재`);
     }
 });
+
+/**
+ * F_OK : 0 존재하는가
+ * R_OK : 4
+ * W_OK : 2
+ * X_OK : 1 읽고 쓸 수 있는가
+ * 0:000
+ * 1:001
+ * 2:010
+ * 4:100
+ * 비트 연산
+ *
+ */
