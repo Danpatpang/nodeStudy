957591566ef30305ea4567aadfb318075f1ac68a
puri8467 <puri8467@gmail.com>
2018-10-08 23:46:09 +0900

    extends&block test code

--- part6/src/routes/test.js
+++ part6/src/routes/test.js
@@ -1,22 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
-router.get(`/`, function(req, res, next) {
-    next('route');
-}, function(req, res, next){
-    console.log("실행 X");
-    next();
-}, function(req, res) {
-    console.log("실행 X");
-    next();
-});
+// router.get(`/`, function(req, res, next) {
+//     next('route');
+// }, function(req, res, next){
+//     console.log("실행 X");
+//     next();
+// }, function(req, res) {
+//     console.log("실행 X");
+//     next();
+// });
+//
+// router.get(`/`, function(req, res) {
+//     console.log(`실행`);
+//     res.render(`index`, {title: `Express`});
+// });
 
-router.get(`/`, function(req, res) {
-    console.log(`실행`);
-    res.render(`index`, {title: `Express`});
-});
-
-router.get(`/call`, function(req, res){
+router.get(`/`, function(req, res){
     res.render('test', {title: "express"});
 })
 module.exports = router;
