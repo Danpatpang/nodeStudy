f60874d549bb72b67733b35f11af1e45363199f6
puri8467 <puri8467@gmail.com>
2018-10-15 04:07:50 +0900

    update part7 source code

--- part7/src/app.js
+++ part7/src/app.js
@@ -8,6 +8,7 @@ var sequelize = require('./models').sequelize;
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var commentsRouter = require('./routes/comments');
 
 var app = express();
 // 서버 실행 시 자동으로 연동
@@ -15,7 +16,7 @@ sequelize.sync();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -25,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/comments', commentsRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- part7/src/routes/index.js
+++ part7/src/routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
+var User = require('../models').User;
+
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    User.findAll()
+        .then((users) => {
+            res.render('sequelize', {users});
+        })
+        .catch((err) => {
+            console.error(err);
+            next(err);
+        });
 });
+
+// router.get('/', async (req, res, next) => {
+//     try {
+//         const users = await User.findAll();
+//         res.render('sequelize', {users});
+//     }
+//     catch (err) {
+//         console.error(err);
+//         next(err);
+//     }
+// });
 
 module.exports = router;
