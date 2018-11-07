b9f0f0bb287b219638f58295353bb8dfbb9f9b5d
danpatpang <puri8467@gmail.com>
2018-11-07 18:11:49 +0900

    edit handlebars

--- newNode/site/app.js
+++ newNode/site/app.js
@@ -1,11 +1,23 @@
 var express = require('express');
-var handlebars = require('express-handlebars');
+var handlebars = require('express-handlebars').create({
+    defaultLayout: 'main',
+    // this._sections는 무엇인가????
+    helpers: {
+        section: (name, options) => {
+            if (!this._sections) {
+                this._sections = {};
+            }
+            this._sections[name] = options.fn(this);
+            return null;
+        }
+    }
+});
 var fortune = require('./lib/fortune');
 
 var app = express();
 app.set('port', process.env.PORT || 3000);
-app.engine('handlebars', handlebars({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
+app.engine('handlebars', handlebars.engine)
+    .set('view engine', 'handlebars');
 
 app.use(express.static(__dirname + '/public'));
 
@@ -17,16 +29,80 @@ app.use((req, res, next) => {
     next();
 });
 
+function getWeatherData() {
+    return {
+        locations: [
+            {
+                name: 'Portland',
+                forecastUrl: 'http://www.wunderground.com/US/OR/Portland.html',
+                iconUrl: 'http:/icons-ak.wxug.com/i/c/k/cloudy.gif',
+                weather: 'Overcast',
+                temp: '54.1F (12.3C)'
+            },
+            {
+                name: 'Bend',
+                forecastUrl: 'http://www.wunderground.com/US/OR/Bend.html',
+                iconUrl: 'http:/icons-ak.wxug.com/i/c/k/partlycloudy.gif',
+                weather: 'Partly Cloudy',
+                temp: '55.0F (12.8C)'
+            },
+            {
+                name: 'Manzanita',
+                forecastUrl: 'http://www.wunderground.com/US/OR/Manzanita.html',
+                iconUrl: 'http:/icons-ak.wxug.com/i/c/k/rain.gif',
+                weather: 'Light Rain',
+                temp: '55.0F (12.8C)'
+            }
+        ]
+    }
+}
+
+app.use((req, res, next) => {
+    if (!res.locals.particals) {
+        res.locals.particals = {};
+    }
+    res.locals.particals.weatherContext = getWeatherData();
+    next();
+});
+
 app.get('/', (req, res) => {
     res.render('home');
 });
 
 app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.get('/error', (req, res) => {
+    res.status(500);
+    res.render('error');
+    // 다음과 동일
+    res.status(500)
+        .render('error');
+});
+
+app.get('/greeting', (req, res) => {
     res.render('about', {
-        fortune: fortune.getFortune(),
-        pageTestScript : "/qa/test-about.js"
-    });
+        message: 'welcome',
+        style: req.query.style,
+        userid: req.cookie.userId,
+        username: req.session.username
+    })
+});
+
+app.get('/no-layout', (req, res) => {
+    res.render('no-layout', {layout: null});
+});
+
+app.get('/custom-layout', (req, res) => {
+    res.render('custom-layout', {layout: 'custom'});
+});
+
+app.get('test', (req, res) => {
+    res.type('text/plain');
+    res.send('this is a test');
 })
+
 
 app.get('/tours/hood-river', (req, res) => {
     res.render('tours/hood-river');
--- part7/src/models/user.js
+++ part7/src/models/user.js
@@ -1,26 +1,26 @@
 module.exports = (sequelize, DataTypes) => {
     return sequelize.define('user', {
         name: {
-            type: DataTypes.STRING(20),
-            allowNUll: false,
-            unique: true,
-        },
-        age: {
-            type: DataTypes.INTEGER.UNSIGNED,
-            allowNUll: false,
-        },
-        married: {
-            type: DataTypes.BOOLEAN,
-            allowNUll: false,
-        },
-        comment: {
-            type: DataTypes.TEXT,
-            allowNull: true,
-        },
-        created_at: {
-            type: DataTypes.DATE,
-            allowNull: false,
-            defaultValue: sequelize.literal('now()'),
-        },
-    }, {timestamps: false});
+    type: DataTypes.STRING(20),
+        allowNUll: false,
+        unique: true,
+},
+age: {
+    type: DataTypes.INTEGER.UNSIGNED,
+        allowNUll: false,
+},
+married: {
+    type: DataTypes.BOOLEAN,
+        allowNUll: false,
+},
+comment: {
+    type: DataTypes.TEXT,
+        allowNull: true,
+},
+created_at: {
+    type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: sequelize.literal('now()'),
+},
+}, {timestamps: false});
 };
